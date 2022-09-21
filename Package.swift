bfc1618abb5d1baf9e908ad3a62cfa812c38fbc9
Olaf Andreas Øvrum <oao@itf-as.no>
2022-09-21 09:43:56 +0200

    Adding iOS 12 support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Netaxept-iOS-SDK",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pia.xcframework",
-            url: "https://github.com/olafandreas-itf/Netaxept-iOS-SDK/releases/download/2.6.3/Pia.xcframework.zip",
+            url: "https://github.com/olafandreas-itf/Netaxept-iOS-SDK/releases/download/2.6.4/Pia.xcframework.zip",
             checksum: "c21f18f47ebc06ee7e380f71adde16947b066df67115bbf8c6af855e753c0dc0"
         ),
     ]
